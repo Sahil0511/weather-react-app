fdbcaa1550a335066e591e091f5637d9f502db7f
Sahil Savardekar <75142268+Sahil0511@users.noreply.github.com>
2023-07-11 02:09:30 +0530

    sttyle:  UI done

--- src/Weather.js
+++ src/Weather.js
@@ -1,7 +1,90 @@
 import React from "react";
+import "./index.css";
 
 const Weather = () => {
-  return <></>;
+  return (
+    <>
+      <div className="wrap">
+        <div className="search">
+          <input
+            type="search"
+            placeholder="search...."
+            autoFocus
+            id="search"
+            className="searchTerm"
+          />
+          <button className="searchButton" type="button">
+            Search
+          </button>
+        </div>
+      </div>
+
+      <article className="widget">
+        <div className="weatherIcon">
+          <i className={"wi wi-day-sunny"}></i>
+        </div>
+
+        <div className="weatherInfo">
+          <div className="temperature">
+            <span>30&deg;</span>
+          </div>
+
+          <div className="description">
+            <div className="weatherCondition">Sunny</div>
+            <div className="place">Mumbai,India</div>
+          </div>
+        </div>
+
+        <div className="date">{new Date().toLocaleString()}</div>
+
+        <div className="extra-temp">
+          <div className="temp-info-minmax">
+            <div className="two-sided-section">
+              <p>
+                <i className={"wi wi-sunset"}></i>
+              </p>
+              <p className="extra-info-leftside">
+                11:11 PM <br />
+                Sunset
+              </p>
+            </div>
+
+            <div className="two-sided-section">
+              <p>
+                <i className={"wi wi-sunset"}></i>
+              </p>
+              <p className="extra-info-leftside">
+                11:11 PM <br />
+                Sunset
+              </p>
+            </div>
+          </div>
+
+          <div className="weather-extra-info">
+            <div className="two-sided-section">
+              <p>
+                <i className={"wi wi-sunset"}></i>
+              </p>
+              <p className="extra-info-leftside">
+                11:11 PM <br />
+                Sunset
+              </p>
+            </div>
+
+            <div className="two-sided-section">
+              <p>
+                <i className={"wi wi-sunset"}></i>
+              </p>
+              <p className="extra-info-leftside">
+                11:11 PM <br />
+                Sunset
+              </p>
+            </div>
+          </div>
+        </div>
+      </article>
+    </>
+  );
 };
 
 export default Weather;
