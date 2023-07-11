86b64e9f752abb4565cfb7d9c28258c9287c99ec
Sahil Savardekar <75142268+Sahil0511@users.noreply.github.com>
2023-07-12 00:54:15 +0530

    docs: destructured object

--- src/Weather.js
+++ src/Weather.js
@@ -3,15 +3,39 @@ import "./index.css";
 
 const Weather = () => {
   const [search, setSearch] = useState("mumbai");
+  const [weatherInfo, setWeatherInfo] = useState({});
 
   const getData = async () => {
     try {
       let url = `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=2d779df20602d276a2f7bfcf68a58b77`;
       let res = await fetch(url);
       let data = await res.json();
-      console.log(data);
+
+      const { temp, humidity, pressure } = data.main;
+      const { main: weatherType } = data.weather[0];
+      const { name } = data;
+      const { country, sunset } = data.sys;
+      const { speed } = data.wind;
+
+      const myNewData = {
+        temp,
+        humidity,
+        pressure,
+        weatherType,
+        name,
+        country,
+        sunset,
+        speed,
+      };
+      setWeatherInfo(myNewData);
     } catch (error) {
       console.log(error);
+    }
+  };
+
+  const enter = (e) => {
+    if (e.keyCode === 13) {
+      getData();
     }
   };
 
@@ -27,6 +51,7 @@ const Weather = () => {
             className="searchTerm"
             value={search}
             onChange={(e) => setSearch(e.target.value)}
+            onKeyDown={enter}
           />
           <button className="searchButton" type="button" onClick={getData}>
             Search
